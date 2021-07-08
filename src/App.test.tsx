b60e8ad26815b57f74bffb595cc9e5eeb7488c2b
bob <bob@bobs-MacBook-Pro.local>
2021-07-08 12:40:10 +0700

    chore: add file test

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+test('renders learn react link', () => {
+	render(<App />)
+	expect(1).toBe(1)
+})
